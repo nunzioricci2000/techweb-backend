6b09dcc43b2d409195f6696ae69b7d9ca90f5dd7
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-08-05 21:06:43 +0200

    Add HttpError base class for HTTP errors
    Introduces a new HttpError class to standardize error handling for HTTP issues. The class supports custom messages, status codes, and additional fields for extensibility.

---
+++ src/errors/http.error.js
@@ -0,0 +1,17 @@
+
+/**
+ * Base errors for http issues
+ */
+export class HttpError extends Error {
+    /**
+     * @param {string} message error message
+     * @param {number} code HTTP status code
+     * @param {Object} custom_fields additional fields to include in the error
+     */
+    constructor(code, message, custom_fields = {}) {
+        super(message);
+        this.name = "HttpError";
+        this.code = code;
+        this.custom_fields = custom_fields;
+    }
+}
