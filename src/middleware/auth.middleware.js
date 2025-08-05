8737ed435b0763ed29cb736c8a1e4296099dc473
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-08-05 19:44:16 +0200

    Add authentication middleware
    Introduces a middleware factory for checking user authentication using a token from the authorization header. Responds with 401 Unauthorized if the token is missing or invalid.

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+
+/**
+ * Factory that creates a middleware for checking that user is authenticated.
+ * @param {import('../controllers/auth.controller').default} authController
+ * @return {Function} Middleware function
+ */
+export default function checkAuth(authController) {
+    /**
+     * Middleware function to check if the user is authenticated.
+     * @param {{ headers: { authorization: string } }} req - The request object.
+     * @param {*} res - The response object.
+     * @param {Function} next - The next middleware function in the stack.
+     * @return {void}
+     */
+    return async function (req, res, next) {
+        const authHeader = req.headers['authorization']
+        const token = authHeader?.split(' ')[1];
+        if (!token) next({ status: 401, message: "Unauthorized" });
+        try {
+            req.username = await authController.verifyToken(token);
+            next();
+        } catch(_) {
+            next({ status: 401, message: "Unauthorized" })
+        }
+    }
+}
