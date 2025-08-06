19a59e3f17d8ed3384a74ff19c3ea949c808201e
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-08-07 00:00:37 +0200

    Refactor auth middleware to use authService
    Replaces usage of authController with authService in the authentication middleware. Updates parameter types and function naming for clarity and consistency.

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,10 +1,10 @@
 
 /**
  * Factory that creates a middleware for checking that user is authenticated.
- * @param {import('../controllers/auth.controller').default} authController
- * @return {Function} Middleware function
+ * @param {import('../services/auth.service').default} authService
+ * @return {import('express').RequestHandler} Middleware function
  */
-export default function checkAuth(authController) {
+export default function CheckAuth(authService) {
     /**
      * Middleware function to check if the user is authenticated.
      * @param {{ headers: { authorization: string } }} req - The request object.
@@ -17,7 +17,7 @@ export default function checkAuth(authController) {
         const token = authHeader?.split(' ')[1];
         if (!token) next({ status: 401, message: "Unauthorized" });
         try {
-            req.user = await authController.verifyToken(token);
+            req.user = await authService.verifyToken(token);
             next();
         } catch(_) {
             next({ status: 401, message: "Unauthorized" })
