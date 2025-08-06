63e55b0554f0ef09736f2a542ed215a876790a14
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-08-06 23:36:47 +0200

    Add environment variable config and new constants
    Imported dotenv to load environment variables and added new constants for DB_URI, JWT_SECRET, HASH_SALT_ROUNDS, and PORT with default values. This centralizes configuration and improves flexibility for different environments.

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,2 +1,8 @@
+import dotenv from "dotenv";
 
-export let HASH_SALT = process.env["HASH_SALT"];
+dotenv.config();
+
+export const DB_URI = process.env["DB_URI"] || "sqlite:mydb.sqlite";
+export const JWT_SECRET = process.env["JWT_SECRET"] || "shhhhhh";
+export const HASH_SALT_ROUNDS = Number(process.env["HASH_SALT_ROUNDS"]) || 10;
+export const PORT = Number(process.env["HASH_SALT_ROUNDS"]) || 3000;
