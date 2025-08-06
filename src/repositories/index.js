52a359426d4395aace4dc73fd654879148cdd8b7
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-08-06 23:37:38 +0200

    Add repository initializer with UserRepository
    Introduces an initRepositories function to initialize and export the UserRepository using the provided model collection. Sets up the structure for managing repositories in the application.

---
+++ src/repositories/index.js
@@ -0,0 +1,18 @@
+import UserRepository from './user.repository.js';
+
+/**
+ * Initializes the repositories for the application.
+ * @param {import('../models/index.js').ModelCollection} modelCollection - The collection of models to initialize repositories with.
+ * @return {Promise<RepositoryCollection>} A promise that resolves to a collection of repositories.
+ */
+export default async function initRepositories(modelCollection) {
+    const { UserModel, RestaurantModel, ReviewModel, VoteModel } = modelCollection;
+    return {
+        userRepository: new UserRepository(UserModel),
+    };
+}
+
+/**
+ * @typedef {object} RepositoryCollection
+ * @property {import('./user.repository.js').default} userRepository - The UserRepository instance.
+ */
