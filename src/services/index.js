b16841d72ef77568d0294969ac934fbe8383c2ec
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-08-28 16:26:48 +0200

    Add RestaurantService to service initialization
    RestaurantService is now imported and initialized in the service collection using restaurantRepository. The ServiceCollection typedef is updated to include restaurantService.

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 import { HASH_SALT_ROUNDS, JWT_SECRET } from '../common/constants.js';
 import AuthService from './auth.service.js';
+import RestaurantService from './restaurant.service.js';
 
 /**
  * Initializes the repositories for the application.
@@ -7,13 +8,15 @@ import AuthService from './auth.service.js';
  * @return {Promise<ServiceCollection>} A promise that resolves to a collection of repositories.
  */
 export default async function initServices(repositoryCollection) {
-    const { userRepository } = repositoryCollection;
+    const { userRepository, restaurantRepository } = repositoryCollection;
     return {
         authService: new AuthService(userRepository, JWT_SECRET, HASH_SALT_ROUNDS),
+        restaurantService: new RestaurantService(restaurantRepository),
     };
 }
 
 /**
  * @typedef {object} ServiceCollection
  * @property {import('./auth.service.js').default} authService - The UserService instance.
+ * @property {import('./restaurant.service.js').default} restaurantService - The RestaurantService instance.
  */
