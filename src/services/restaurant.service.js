a313bfe9ec3cefe2f40268fb2cc20c827245120b
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-08-28 16:26:36 +0200

    Add RestaurantService for CRUD operations
    Introduces RestaurantService class to handle creation, reading, updating, and deletion of restaurant entities. Includes owner validation for update and delete operations, and delegates data access to the restaurant repository.

---
+++ src/services/restaurant.service.js
@@ -0,0 +1,90 @@
+export default class RestaurantService {
+    /**
+     * @type {import('../repositories/restaurant.repository.js').default}
+     */
+    #restaurantRepository;
+
+    /**
+     * Creates an instance of RestaurantService.
+     * @param {import('../repositories/restaurant.repository.js').default} restaurantRepository
+     */
+    constructor(restaurantRepository) {
+        this.#restaurantRepository = restaurantRepository;
+    }
+
+    /**
+     * Creates a restaurant.
+     * @param {string} name - The name of the restaurant
+     * @param {string} description - The description of the restaurant
+     * @param {{latitude: number, longitude: number}} location - The geographical location of
+     * the restaurant
+     * @param {string} imageUrl - The URL of the restaurant's image
+     * @param {number} ownerId
+     * @returns {Promise<import('../repositories/restaurant.repository.js').Restaurant>} The restaurant object created
+     */
+    async createRestaurant(name, description, location, imageUrl, ownerId) {
+        return this.#restaurantRepository.createRestaurant(
+            name,
+            description,
+            location,
+            imageUrl,
+            ownerId
+        );
+    }
+
+    /**
+     * Reads a restaurant by ID.
+     * @param {number} id - The ID of the restaurant to read
+     * @returns {Promise<import('../repositories/restaurant.repository.js').Restaurant?>} The
+     * restaurant object found or null if not found
+     */
+    async readRestaurant(id) {
+        return this.#restaurantRepository.readRestaurant(id);
+    }
+
+    /**
+     * Reads restaurants by filter if given, else returns any restaurant.
+     * @param {{byName: string}?} [filter] - The filter to apply when reading restaurants
+     * @returns {Promise<import('../repositories/restaurant.repository.js').Restaurant[]>} The list of restaurant objects found
+     */
+    async readRestaurants(filter = null) {
+        return this.#restaurantRepository.readRestaurants(filter);
+    }
+
+    /**
+     * Updates a restaurant by ID.
+     * @param {number} id - The ID of the restaurant to update
+     * @param {Partial<import('../repositories/restaurant.repository.js').Restaurant> } updates - The updates to apply to the restaurant
+     * @param {number} ownerId - The ID of the owner of the restaurant
+     * @returns {Promise<import('../repositories/restaurant.repository.js').Restaurant>} The updated
+     * restaurant object
+     */
+    async updateRestaurant(id, updates, ownerId) {
+        const restaurant = await this.#restaurantRepository.readRestaurant(id);
+        if (!restaurant) throw new Error(`Restaurant with ID ${id} not found`);
+        if (restaurant.ownerId !== ownerId) {
+            throw new Error(`User with ID ${ownerId} is not the owner of restaurant with ID ${id}`);
+        }
+        const updatedRestaurant = {
+            ...restaurant,
+            ...updates,
+        };
+
+        return this.#restaurantRepository.updateRestaurant(id, updatedRestaurant);
+    }
+
+    /**
+     * Deletes a restaurant by ID.
+     * @param {number} id - The ID of the restaurant to delete
+     * @param {number} ownerId - The ID of the owner of the restaurant
+     * @returns {Promise<void>}
+     */
+    async deleteRestaurant(id, ownerId) {
+        const restaurant = await this.#restaurantRepository.readRestaurant(id);
+        if (!restaurant) throw new Error(`Restaurant with ID ${id} not found`);
+        if (restaurant.ownerId !== ownerId) {
+            throw new Error(`User ${ownerId} is not the owner of restaurant with ID ${id}`);
+        }
+        await this.#restaurantRepository.deleteRestaurant(id);
+    }
+}
