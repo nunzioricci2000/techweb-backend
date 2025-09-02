d633b100d4d3d095112856eea715a511e37341da
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-09-02 17:43:49 +0200

    Add ReviewRepository to repository initialization
    Integrates ReviewRepository into the initRepositories function and updates the RepositoryCollection typedef to include reviewRepository. This allows the application to manage review data alongside users and restaurants.

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,5 +1,6 @@
 import RestaurantRepository from './restaurant.repository.js';
 import UserRepository from './user.repository.js';
+import ReviewRepository from './review.repository.js';
 
 /**
  * Initializes the repositories for the application.
@@ -7,10 +8,11 @@ import UserRepository from './user.repository.js';
  * @return {Promise<RepositoryCollection>} A promise that resolves to a collection of repositories.
  */
 export default async function initRepositories(modelCollection) {
-    const { UserModel, RestaurantModel } = modelCollection;
+    const { UserModel, RestaurantModel, ReviewModel } = modelCollection;
     return {
         userRepository: new UserRepository(UserModel),
         restaurantRepository: new RestaurantRepository(RestaurantModel),
+        reviewRepository: new ReviewRepository(ReviewModel),
     };
 }
 
@@ -18,4 +20,5 @@ export default async function initRepositories(modelCollection) {
  * @typedef {object} RepositoryCollection
  * @property {import('./user.repository.js').default} userRepository - The UserRepository instance.
  * @property {import('./restaurant.repository.js').default} restaurantRepository - The RestaurantRepository instance.
+ * @property {import('./review.repository.js').default} reviewRepository - The ReviewRepository instance.
  */
